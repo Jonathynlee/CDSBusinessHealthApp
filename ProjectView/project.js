faa5dd6dc51cd7b8b2c3298a251aaf5d8f132b71
jleemajor <jonathynlee@gmail.com>
2019-10-31 00:29:09 -0700

    adding pin images

--- ProjectView/project.js
+++ ProjectView/project.js
@@ -1,21 +1,22 @@
 
-var APIProjects=[];
-var usefulArray=[];
-var memberInfo=[];
+var APIProjects = [];
+var usefulArray = [];
+var memberInfo = [];
 
- 
-statusAPI=["on schedule","falling behind","overdue"];
-priority=["high","medium","low"];
+
+statusAPI = ["on schedule", "falling behind", "overdue"];
+priority = ["high", "medium", "low"];
 
 var accountID = "3736340";
-var clientID="f83106024123731e9dd7114e6a6361bd81ba241f";
-var clientSecret="0a18b9b736b153a492513183f1992346f094b8c7";
+var clientID = "f83106024123731e9dd7114e6a6361bd81ba241f";
+var clientSecret = "0a18b9b736b153a492513183f1992346f094b8c7";
 
 var redirectURI = "https://jonathynlee.github.io/CDSBusinessHealthApp/ProjectView/index.html";
 var token;
 
 var queryURL = "https://3.basecampapi.com/";
 var code;
+var globalData;
 
 jQuery.ajaxPrefilter(function (options) {
     if (options.crossDomain && jQuery.support.cors) {
@@ -51,12 +52,12 @@ function getToken() {
         success: function (response) {
             //console.log(response)
             token = response.access_token;
-            localStorage.setItem("token",token);
-           // console.log(token)
+            localStorage.setItem("token", token);
+            // console.log(token)
             //createProject();
             getAllProjects();
-            
-            
+
+
 
         }
     })
@@ -64,104 +65,104 @@ function getToken() {
 
 
 
- /////////////////////////////////////////////////////////////////////
- 
- function getToDoSets(){
-     
-     for (var i=0;i<APIProjects.length;i++){//APIProjects.length;
-         var u=APIProjects[i].url;
-         //console.log(u);
-         $.ajax({
+/////////////////////////////////////////////////////////////////////
+
+function getToDoSets() {
+
+    for (var i = 0; i < APIProjects.length; i++) {//APIProjects.length;
+        var u = APIProjects[i].url;
+        //console.log(u);
+        $.ajax({
             contentType: 'application/json;charset=utf-8',
-    
+
             //processData:false, 
-    
+
             type: "GET",
             headers: {
                 "Authorization": "Bearer " + token
             },
-            url:u,
+            url: u,
             datatype: 'json',
-    
+
             error: function (request, textStatus, errorThrown) {
                 alert(errorThrown);
-    
+
             },
             success: function (response) {
-                r=JSON.stringify(response);
+                r = JSON.stringify(response);
                 //console.log(r);
-               
-                toDoSets=JSON.parse(r);
-                var id=toDoSets.id
-                var completed=toDoSets.completed;
-                var ratio=toDoSets.completed_ratio;
-                var listCount=toDoSets.todolists_count;
-                var todolistsUrl=toDoSets.todolists_url;
+
+                toDoSets = JSON.parse(r);
+                var id = toDoSets.id
+                var completed = toDoSets.completed;
+                var ratio = toDoSets.completed_ratio;
+                var listCount = toDoSets.todolists_count;
+                var todolistsUrl = toDoSets.todolists_url;
                 //console.log(todolistsUrl); 
 
-                var entry={
-                    id:id,
-                    completed:completed,
-                    ratio:ratio,
-                    listCount:listCount,
-                    todolistsUrl:todolistsUrl
+                var entry = {
+                    id: id,
+                    completed: completed,
+                    ratio: ratio,
+                    listCount: listCount,
+                    todolistsUrl: todolistsUrl
                 }
                 usefulArray.push(entry);
-                
-                
-             }    
-           });
-           getProjectMembers(i);//get project members
-        } 
-        
-        
- }
- /////////////////////////////////////////////////////////////////////
- 
- function getProjectMembers(i){
-        var pID=APIProjects[i].ID;
-        var u= queryURL + accountID + "/projects/"+pID+"/people.json";
-        //console.log(pID);
-         $.ajax({
-           contentType: 'application/json;charset=utf-8',
-   
-           //processData:false, 
-   
-           type: "GET",
-           headers: {
-               "Authorization": "Bearer " + token
-           },
-           url:u,
-           datatype: 'json',
-   
-           error: function (request, textStatus, errorThrown) {
-               alert(errorThrown);
-   
-           },
-           success: function (response) {
-                // console.log(response);
-               for (var k=0;k<response.length;k++){
-                   var employee={
-                       id:response[k].id,
-                       name:response[k].name,
-                       emailAddress:response[k].email_address,
-                       title:response[k].title,
-                       personableType:response[k].personable_type
 
-                   }
-                   memberInfo.push(employee);
-               }
-               APIProjects[i].members.push(memberInfo);
-               memberInfo=[];
-               //console.log("Project:"+i+":"+JSON.stringify(APIProjects[i].members));
-               
-            }    
-          });
-        
 
- }
- /////////////////////////////////////////////////////////////////////
- function getAllProjects() {
+            }
+        });
+        getProjectMembers(i);//get project members
+    }
+
+
+}
+/////////////////////////////////////////////////////////////////////
+
+function getProjectMembers(i) {
+    var pID = APIProjects[i].ID;
+    var u = queryURL + accountID + "/projects/" + pID + "/people.json";
+    //console.log(pID);
+    $.ajax({
+        contentType: 'application/json;charset=utf-8',
+
+        //processData:false, 
+
+        type: "GET",
+        headers: {
+            "Authorization": "Bearer " + token
+        },
+        url: u,
+        datatype: 'json',
+
+        error: function (request, textStatus, errorThrown) {
+            alert(errorThrown);
+
+        },
+        success: function (response) {
+            // console.log(response);
+            for (var k = 0; k < response.length; k++) {
+                var employee = {
+                    id: response[k].id,
+                    name: response[k].name,
+                    emailAddress: response[k].email_address,
+                    title: response[k].title,
+                    personableType: response[k].personable_type
+
+                }
+                memberInfo.push(employee);
+            }
+            APIProjects[i].members.push(memberInfo);
+            memberInfo = [];
+            //console.log("Project:"+i+":"+JSON.stringify(APIProjects[i].members));
+
+        }
+    });
+
+
+}
+/////////////////////////////////////////////////////////////////////
+function getAllProjects() {
 
     $.ajax({
         contentType: 'application/json;charset=utf-8',
@@ -180,62 +181,62 @@ function getToken() {
 
         },
         success: function (response) {
-           
-            for (var i=0;i<response.length;i++){
-                projectAPI=new Object();
-                projectAPI.ID=response[i].id;
-                projectAPI.name=response[i].name;
-                projectAPI.url=response[i].dock[2].url;
-                projectAPI.status=statusAPI[randomNumberGenerator()];
-                projectAPI.priority=priority[randomNumberGenerator()];
-                projectAPI.pinned=false;
-                projectAPI.members=[];//////////////////////////////////////////
+
+            for (var i = 0; i < response.length; i++) {
+                projectAPI = new Object();
+                projectAPI.ID = response[i].id;
+                projectAPI.name = response[i].name;
+                projectAPI.url = response[i].dock[2].url;
+                projectAPI.status = statusAPI[randomNumberGenerator()];
+                projectAPI.priority = priority[randomNumberGenerator()];
+                projectAPI.pinned = false;
+                projectAPI.members = [];//////////////////////////////////////////
                 APIProjects.push(projectAPI);
-               
+
             }
             //console.log(APIProjects);
             viewProjects();
             getToDoSets();
-            
+
         }
     });
-    
 
- }
- ////////////////////////////////////////////////////////////////////
- var localToken = JSON.stringify(localStorage.token);
- if (localToken != "null"){ 
- 
-     token = localToken;
-     getAllProjects();
-     }else{
- getCode();
- if (!code) {
-     //authorize();
- } else {
-     
-     //getToken();
- }
- }
+
+}
+////////////////////////////////////////////////////////////////////
+var localToken = JSON.stringify(localStorage.token);
+if (localToken != "null") {
+
+    token = localToken;
+    getAllProjects();
+} else {
+    getCode();
+    if (!code) {
+        //authorize();
+    } else {
+
+        //getToken();
+    }
+}
 //////////////////////////////////////////////////////////////////////////////////////////////////
-function randomNumberGenerator(){
-    return Math.floor(Math.random()*3);
+function randomNumberGenerator() {
+    return Math.floor(Math.random() * 3);
 }
 
 ///VIEW MEMBER INFO OF A PROJECT//////////
 function viewMember(index) {
-      var project=APIProjects[index];
-      $("#mbody").empty();
-      var r=JSON.stringify(project.members);
-       var o= JSON.parse(r)
-      for(var v=0;v<o[0].length;v++) {
-       var memberID = o[0][v].id;
-       var memberName = o[0][v].name;
-       var memberEmail = o[0][v].emailAddress;
-       var memberPersonableType = o[0][v].personableType;
-       var memberTitle = o[0][v].title;
+    var project = APIProjects[index];
+    $("#mbody").empty();
+    var r = JSON.stringify(project.members);
+    var o = JSON.parse(r)
+    for (var v = 0; v < o[0].length; v++) {
+        var memberID = o[0][v].id;
+        var memberName = o[0][v].name;
+        var memberEmail = o[0][v].emailAddress;
+        var memberPersonableType = o[0][v].personableType;
+        var memberTitle = o[0][v].title;
 
-       var row = $("<tr>");
+        var row = $("<tr>");
         var data1 = $("<th scope='row'>" + memberID + "</th>");
         row.append(data1);
 
@@ -252,12 +253,12 @@ function viewMember(index) {
         $(row).append(data1);
 
         data1 = $("<td>");
-        data1.append("<span>" + memberTitle+ "</span>");
+        data1.append("<span>" + memberTitle + "</span>");
         $(row).append(data1);
 
         $("#mbody").append(row);
-      }  
- 
+    }
+
 }
 
 /////////////resize modal////////////
@@ -271,33 +272,40 @@ $('#myModal2').on('show', function () {
 //////////When Project Button is Clicked the asignees and number of tasks that are open will be listed.////////
 function viewTasks(index) { //index
 
-       var project = usefulArray[index];
-       $("#tbody").empty();
-    
-        var id = project.id;
-        var completed= project.completed;
-        var ratio=project.ratio;
-        var listCount=project.listCount;
+    var project = usefulArray[index];
+    $("#tbody").empty();
 
-        var row = $("<tr>");
-        var index = $("<th scope='row'>" + id+ "</th>");
-        row.append(index);
-        var data1 = $("<td>");
-        data1.append("<span>" + completed + "</span>");
-        $(row).append(data1);
+    var id = project.id;
+    var completed = project.completed;
+    var ratio = project.ratio;
+    var listCount = project.listCount;
 
-        data1 = $("<td>");
-        data1.append("<span>" + ratio+ "</span>");
-        $(row).append(data1);
+    var row = $("<tr>");
+    var index = $("<th scope='row'>" + id + "</th>");
+    row.append(index);
+    var data1 = $("<td>");
+    data1.append("<span>" + completed + "</span>");
+    $(row).append(data1);
 
-        data1 = $("<td>");
-        data1.append("<span>" + listCount+ "</span>");
-        $(row).append(data1);
+    data1 = $("<td>");
+    data1.append("<span>" + ratio + "</span>");
+    $(row).append(data1);
 
-        $("#tbody").append(row);
+    data1 = $("<td>");
+    data1.append("<span>" + listCount + "</span>");
+    $(row).append(data1);
+
+    $("#tbody").append(row);
 
 }
 ////////////////////////////////////////////////////////////////
+// LOOK FOR GLOBAL DATA
+
+globalData = JSON.stringify(localStorage.getItem(globalData));
+if (globalData != "null"){
+
+}
+
 //VIEW PROJECTS,THEIR STATUS AND PRIORITY
 function viewProjects() {
 
@@ -305,6 +313,21 @@ function viewProjects() {
 
         var row = $("<tr>");
         var index = $("<th scope='row'>" + APIProjects[i].ID + "</th>");
+        //ADD PINS AND DATA
+        if(globalData != "null"){
+            var pinned = globalData.projectData[i].pinned;
+
+        }else{
+            var pinned = false;
+        }
+        if (pinned){
+            var pinImage = "assets/images/pin.png";
+        }else{
+            var pinImage = "assets/images/unpin.png";
+        }
+        var pinButton = $("<button class = \"pinButton\" pinned = \""+pinned+"\" style = \"background-image:url(\""+pinImage+"\");\"></button>")
+        index.append(pinButton);
+
         row.append(index);
         var str = "viewTasks(" + i + ")";
 
