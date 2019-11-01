871f954c1d3af275dea89ad3bcfe8bf250640cf2
jleemajor <jonathynlee@gmail.com>
2019-10-31 19:09:17 -0700

    changed jquery source

--- finance/financeScript.js
+++ finance/financeScript.js
@@ -4,7 +4,7 @@ var clientID = "AB6Ss4YTrwuhkywv8RR1mF6odU3yQqDThZ5hutPqXRLq7xxn2F";
 var clientSecret = "UXNg2qhxPyrFYHXzYd1Q4DmISm0BUki3Z7GxqIVu";
 var redirectURI = "https://jonathynlee.github.io/CDSBusinessHealthApp/finance/finance.html";
 var scope = "com.intuit.quickbooks.accounting"
-var queryUrl = "https://appcenter.intuit.com/connect/oauth2" ;
+var queryUrl = "https://appcenter.intuit.com/connect/oauth2";
 var responseType = "code"
 var realmid;
 var authCode;
@@ -17,43 +17,43 @@ function getUrlParameter(name) {
     var results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
-console.log("Authorization URL: "+queryUrl+"?client_id="+clientID+"&response_type="+responseType+"&scope="+scope+"&redirect_uri="+redirectURI+"&state=security_token");
+console.log("Authorization URL: " + queryUrl + "?client_id=" + clientID + "&response_type=" + responseType + "&scope=" + scope + "&redirect_uri=" + redirectURI + "&state=security_token");
 
-function authorize(){
-    window.location = queryUrl+"?client_id="+clientID+"&response_type="+responseType+"&scope="+scope+"&redirect_uri="+redirectURI+"&state=security_token";
+function authorize() {
+    window.location = queryUrl + "?client_id=" + clientID + "&response_type=" + responseType + "&scope=" + scope + "&redirect_uri=" + redirectURI + "&state=security_token";
 }
 
-function setAuth(){
-realmid = getUrlParameter("realmid");
-authCode = getUrlParameter("code");
-var state = getUrlParameter("state"); 
-getAuthToken();
+function setAuth() {
+    realmid = getUrlParameter("realmid");
+    authCode = getUrlParameter("code");
+    var state = getUrlParameter("state");
+    getAuthToken();
 }
 
-function getAuthToken(){
-$.ajax({
-    type: "POST",
-    url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer?code="+authCode+"&redirect_uri="+redirectURI+"&grant_type=authorization_code",
-    success: function (response) {
-        //console.log(response)
-        authToken = response.access_token;
-        localStorage.setItem("financeToken", authToken);
-        // console.log(token)
-        //createProject();
-        
+function getAuthToken() {
+    $.ajax({
+        type: "POST",
+        url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer?code=" + authCode + "&redirect_uri=" + redirectURI + "&grant_type=authorization_code",
+        success: function (response) {
+            //console.log(response)
+            authToken = response.access_token;
+            localStorage.setItem("financeToken", authToken);
+            // console.log(token)
+            //createProject();
 
 
 
+
+        }
+    })
+}
+
+if (authToken == undefined) {
+    if (getUrlParameter("code") === "") {
+        authorize();
+    } else {
+        setAuth();
     }
-})
-}
-
-if (authToken == undefined){
-if (getUrlParameter("code")===""){
-authorize();
-}else{
-    setAuth();
-}
-}else{
-localStorage.setItem("financeToken", authToken);
+} else {
+    localStorage.setItem("financeToken", authToken);
 }
